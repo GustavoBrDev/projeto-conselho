ef2e2d4e96cb7f979704879bc6ce5a984f769213
Gustavo Stinghen <gustavobr.developer@gmail.com>
2025-03-05 18:34:47 -0300

    Commit inicial
    Commit inicial do projeto. Feito a configuração básica do POM.

---
+++ src/main/java/conselho/estudante/com/projetoconselho/ProjetoConselhoApplication.java
@@ -0,0 +1,13 @@
+package conselho.estudante.com.projetoconselho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConselhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoConselhoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/conselho/estudante/com/projetoconselho/ProjetoConselhoApplicationTests.java
@@ -0,0 +1,13 @@
+package conselho.estudante.com.projetoconselho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoConselhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
