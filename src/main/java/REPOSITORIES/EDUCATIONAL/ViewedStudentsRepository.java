a4ebfc36bca8dea38f2519eb1bdfb47d0aeaddcc
akalizik <cauajdutra@gmail.com>
2025-03-18 16:01:10 -0300

    🔧 refactor: Adicionado método findByCouncil ao ViewedStudentsRepository
    - Adicionado o método `findByCouncil(Council council)` para buscar `ViewedStudents` associados a um `Council`.
    - Melhorando a recuperação de dados de alunos visualizados.

--- src/main/java/REPOSITORIES/EDUCATIONAL/ViewedStudentsRepository.java
+++ src/main/java/REPOSITORIES/EDUCATIONAL/ViewedStudentsRepository.java
@@ -1,7 +1,12 @@
 package REPOSITORIES.EDUCATIONAL;
 
 import MODELS.ENTITY.EDUCATIONAL.ViewedStudents;
+import MODELS.ENTITY.EDUCATIONAL.Council;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ViewedStudentsRepository extends JpaRepository<ViewedStudents, Long> {
+
+        ViewedStudents findByCouncil(Council council);
 }
