f4c35da45e0d55f51ea216b425485d4209d4abcd
JoanaVoigt <147541974+JoanaVoigt@users.noreply.github.com>
2025-03-19 16:56:29 -0300

    Criação de Course
    Foram feitas as classes:
    -> CourseRepository
    ->CourseService (falta fazer o filter)

--- src/main/java/conselho/estudante/com/projetoconselho/REPOSITORIES/ADMINISTRATION/CourseRepository.java
+++ src/main/java/conselho/estudante/com/projetoconselho/REPOSITORIES/ADMINISTRATION/CourseRepository.java
@@ -2,6 +2,20 @@ package conselho.estudante.com.projetoconselho.REPOSITORIES.ADMINISTRATION;
 
 import conselho.estudante.com.projetoconselho.MODELS.ENTITY.ADMINISTRATION.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+/**
+ * Repositório para a entidade {@link Course}.
+ *
+ * @author joana voigt
+ * @since 19/03/2025
+ */
+public interface CourseRepository extends JpaRepository<Course, Long>, JpaSpecificationExecutor<Course> {
+    /**
+     * Verifica se um curso com o nome especificado já existe no banco de dados.
+     *
+     * @param name Nome do curso a ser verificado.
+     * @return {@code true} se um curso com o nome fornecido existir, {@code false} caso contrário.
+     */
+    public boolean existsByName(String name);
 
-public interface CourseRepository extends JpaRepository<Course, Long> {
 }
