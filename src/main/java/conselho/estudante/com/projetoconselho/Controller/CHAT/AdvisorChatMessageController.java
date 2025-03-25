51f773fa3ca7157e32e24679d098650574b0bdba
akalizik <cauajdutra@gmail.com>
2025-03-24 21:08:40 -0300

    🔧 refactor: documentação do Swagger feita
    - Tinha esquecido de fazer a documentação em swagger da classe de AdvisiorChatMessageController

--- src/main/java/conselho/estudante/com/projetoconselho/Controller/CHAT/AdvisorChatMessageController.java
+++ src/main/java/conselho/estudante/com/projetoconselho/Controller/CHAT/AdvisorChatMessageController.java
@@ -4,6 +4,7 @@ import conselho.estudante.com.projetoconselho.MODELS.DTO.REQUEST.CHAT.AdvisorCha
 import conselho.estudante.com.projetoconselho.MODELS.DTO.RESPONSE.ChatMessageResponseDTO;
 import conselho.estudante.com.projetoconselho.SERVICES.CHAT.AdvisorChatMessageService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -33,7 +34,12 @@ public class AdvisorChatMessageController {
     @ApiResponse(responseCode = "400", description = "Erro ao criar mensagem de chat de orientadores")
     @ApiResponse (responseCode = "500", description = "Erro interno do servidor")
     @PostMapping
-    public ResponseEntity<ChatMessageResponseDTO> postMessage(@RequestBody @Valid AdvisorChatMessageRequestDTO message) {
+    public ResponseEntity<ChatMessageResponseDTO> postMessage(
+            @Parameter(description = "Mensagem de chat de orientadores", content =
+                    @Content(schema = @Schema(implementation = AdvisorChatMessageRequestDTO.class)),
+                    required = true, example = "{" +
+                    "\"text\": \"Hello, world!\", \"advisor\": 1}")
+            @RequestBody @Valid AdvisorChatMessageRequestDTO message) {
         try {
             return new ResponseEntity<>(service.create(message), HttpStatus.CREATED);
         } catch (Exception e) {
@@ -49,7 +55,12 @@ public class AdvisorChatMessageController {
     @ApiResponse (responseCode = "400", description = "Erro ao buscar mensagens de chat de orientadores")
     @ApiResponse (responseCode = "500", description = "Erro interno do servidor")
     @GetMapping
-    public ResponseEntity<Page<ChatMessageResponseDTO>> findAll(@PageableDefault(size = 20, sort = "timestamp") Pageable pageable) {
+    public ResponseEntity<Page<ChatMessageResponseDTO>> findAll(
+            @Parameter(description = "Filtro de mensagens de chat de orientadores", content =
+                    @Content(schema = @Schema(implementation = ChatMessageResponseDTO.class)),
+                    example = "{\"id\": 1, \"text\": \"Hello, world!\", \"advisor\": 1, \"timestamp\": " +
+                            "\"2025-01-01T00:00:00\", \"isRead\": true, \"isDeleted\": false, \"deletedAt\": null}")
+            @PageableDefault(size = 20, sort = "timestamp") Pageable pageable) {
         try {
             return new ResponseEntity<>(service.findAll(pageable), HttpStatus.OK);
         } catch (Exception e) {
@@ -66,7 +77,9 @@ public class AdvisorChatMessageController {
     @ApiResponse (responseCode = "400", description = "Erro ao buscar mensagens de chat de orientadores")
     @ApiResponse (responseCode = "500", description = "Erro interno do servidor")
     @GetMapping("/student/{id}")
-    public ResponseEntity<Page<ChatMessageResponseDTO>> findByAdvisor(@PathVariable Long id, @PageableDefault(size = 20, sort = "timestamp") Pageable pageable) {
+    public ResponseEntity<Page<ChatMessageResponseDTO>> findByAdvisor(
+            @Parameter(description = "ID do orientador", required = true, example = "1")
+            @PathVariable Long id, @PageableDefault(size = 20, sort = "timestamp") Pageable pageable) {
         try {
             return new ResponseEntity<>(service.findByAdvisor(id, pageable), HttpStatus.OK);
         } catch (Exception e) {
@@ -82,7 +95,9 @@ public class AdvisorChatMessageController {
     @ApiResponse (responseCode = "400", description = "Erro ao buscar mensagem de chat de orientadores")
     @ApiResponse (responseCode = "500", description = "Erro interno do servidor")
     @GetMapping("{/id}")
-    public ResponseEntity<ChatMessageResponseDTO> findById(@PathVariable Long id) {
+    public ResponseEntity<ChatMessageResponseDTO> findById(
+            @Parameter(description = "ID da mensagem de chat de orientadores", required = true, example = "1")
+            @PathVariable Long id) {
         try {
             return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
         } catch (Exception e) {
@@ -98,7 +113,9 @@ public class AdvisorChatMessageController {
     @ApiResponse (responseCode = "400", description = "Erro ao deletar mensagem de chat de orientadores")
     @ApiResponse (responseCode = "500", description = "Erro interno do servidor")
     @DeleteMapping("{/id}")
-    public ResponseEntity<ChatMessageResponseDTO> delete(@PathVariable Long id) {
+    public ResponseEntity<ChatMessageResponseDTO> delete(
+            @Parameter(description = "ID da mensagem de chat de orientadores", required = true, example = "1")
+            @PathVariable Long id) {
         try {
             return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
         } catch (Exception e) {
