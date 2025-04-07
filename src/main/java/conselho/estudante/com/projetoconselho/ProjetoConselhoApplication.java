617e657bbd855900e1bb5661d2d37c37bc2caf04
Gustavo Stinghen <gustavobr.developer@gmail.com>
2025-04-07 16:45:10 -0300

    Criação de ShiftLogsController
    Criação da controller de turnos e adicionado a respectiva tag ao Swagger na classe principal.

--- src/main/java/conselho/estudante/com/projetoconselho/ProjetoConselhoApplication.java
+++ src/main/java/conselho/estudante/com/projetoconselho/ProjetoConselhoApplication.java
@@ -41,7 +41,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         @Tag(name = "Logs de Feedbacks", description = "Recurso de gerenciamento de logs de Feedbacks"),
         @Tag(name = "Logs de Logins", description = "Recurso de gerenciamento de logs de Logins"),
         @Tag(name = "Logs de PreCouncils", description = "Recurso de gerenciamento de logs de PreCouncils"),
-        @Tag(name = "Logs de representantes", description = "Recurso de gerenciamento de logs de representantes")
+        @Tag(name = "Logs de representantes", description = "Recurso de gerenciamento de logs de representantes"),
+        @Tag(name = "Logs de Shifts", description = "Recurso de gerenciamento de logs de Shifts")
 }, servers = {
         @Server(
                 url = "http://localhost:9090",
